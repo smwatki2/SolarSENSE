4f45cbca2b051617c069900ef3869964b9c1fa5a
tcyubahi <tcyubahi@asu.edu>
2018-10-25 22:15:44 -0700

    Completed task #42 - Writing data to persistent storage. Added a guide for configuring mondo dtabase and modified the daemon code to report sensor name and it mac address, plus the timestamp of data collection

---
+++ Guides/MongoDBSetUp.md
@@ -0,0 +1,56 @@
+This guide outlines the steps needed to setup the MongoDB Connection for storing the sensor data
+
+1. Run the following to install MongoDB on RPI
+	
+	```
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get install mongodb-server
+	```
+
+2. To make sure mongodb runs on RPI start up
+	
+	```sudo service mongodb start```
+
+
+3. To test that MongoDB has been installed successfully, run 
+
+	```mongo```
+
+	This will run the MongoDB shell where you can issue mongoDB commands
+
+
+5. Create a new database with name "solarsensereports"
+		
+		Run: 
+
+		```use solarsensereports```
+
+		after the database has been created, exit the mongodb shell by running: 
+
+		```exit```
+
+5. Install the Python MongoDB driver (PyMongo)
+
+	```python3 -m pip install pymongo==3.4.0```
+
+	Opted to use 3.4.0 because the latest (3.7.2) was having problems connecting with the latest mongo release
+
+6. Run the python daemon from folder SensorReader, sensorReader.py, and make sure there is data reported 	at least once so you can test the next step
+
+6. Sample code to use to test if the daemon is saving to the database:
+
+	```
+	mongo
+	use solarsensereports
+	db.reports.find()```
+
+	If the daemon has saved data at least once, you will see a json object printed on screen with the data
+
+	Run ```exit``` to log out of the mongoDB shell
+
+Author: ASU Capstone Team 2018
+Date Modified: 10.25.2018
+
+Glossary:
+	RPI : Raspberry Pi
--- SensorReader/sensorReader.py
+++ SensorReader/sensorReader.py
@@ -1,13 +1,23 @@
-# Author: Tresor Cyubahiro
-# Date: 10.19.2018
-# Description: A program that subscribes to sensor topic and retrieves data sent#              by the sensors
+# Author: ASU Capstone Team 2018
+# Date: 10.25.2018
+# Description: A program that subscribes to sensor topic and retrieves data sent
+#              by the sensors, and saves the data to a MongoDB database
 # SolarSENSE Capstone Project
 
 
 #Install package as 'pip install paho-mqtt'
 import paho.mqtt.client as mqtt 
+import pymongo
+from pymongo import MongoClient
 import json
 import datetime
+
+# declare mongo client on port 27017
+mongoClient = MongoClient('localhost', 27017)
+# declare databse object for database 'solarsensereports'
+db = mongoClient['solarsensereports']
+# access sensor data collection
+reports = db.reports
 
 file = open("sensorReportData.txt", "a")
 
@@ -19,13 +29,13 @@ def on_message(client, data, message):
     jsonData = message.payload.decode("utf-8", "ignore")
     reading = json.loads(jsonData) 
 
-    date = datetime.datetime.now()
-
     # Write Sensor Data and Date to file
-    file.write("Date: "+str(date)+", Data: "+jsonData+"\n")
+    file.write(jsonData+"\n")
+    # Save sensor data to database
+    result = reports.insert_one(reading)
 
     print("Reading from topic: "+message.topic+"\n")
-    print("Moisture: {} \nConductivity {}\nLight: {}\nTemperature: {} \nBattery: {}\n ".format(reading["moisture"], reading["conductivity"], reading["light"], reading["temperature"], reading["battery"]))
+    print("Moisture: {} \nConductivity {}\nLight: {}\nTemperature: {} \nBattery: {}\n Sensor: {}\n Time: {}\n MAC ADDRESS: {}\n".format(reading["moisture"],reading["conductivity"], reading["light"], reading["temperature"], reading["battery"], reading["name_pretty"], reading["timestamp"], reading["mac"]))
 
 client = mqtt.Client()
 client.on_connect = on_connect
