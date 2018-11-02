bce794821498ec547087a16ad937070af3d73680
wjd345 <wjdavis1@asu.edu>
2018-11-01 23:31:57 -0700

    Restructure of Web Server to better follow MVC

---
+++ setup/app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from app.forms import HomeForm
+from flask import render_template
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    hello = "Welcome to SolarSENSE!!!"
+    form = HomeForm()
+    if form.validate_on_submit():
+        if form.generateData.data:
+            return render_template('index.html', hello=hello + "It's pretty cool", form= form)
+
+    return render_template('index.html', hello=hello, form=form)
