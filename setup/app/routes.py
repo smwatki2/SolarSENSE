17848871a07569f95fd26531e6f0120e8fa1ea51
wjd345 <wjdavis1@asu.edu>
2018-11-04 13:22:38 -0700

    Fixed references for new file structure

--- setup/app/routes.py
+++ setup/app/routes.py
@@ -1,3 +1,4 @@
+import os
 from app import app
 from app.forms import HomeForm
 from flask import render_template
@@ -9,6 +10,8 @@ def home():
     form = HomeForm()
     if form.validate_on_submit():
         if form.generateData.data:
-            return render_template('index.html', hello=hello + "It's pretty cool", form= form)
+            # print(subprocess.check_output(['ehco', 'hello world']))
+            os.system('mkdir "testDir"')
+            return render_template('index.html', hello=hello + "It's pretty cool", form=form)
 
     return render_template('index.html', hello=hello, form=form)
