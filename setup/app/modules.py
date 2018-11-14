c5cb26bd9b76706e96049c6fe644694b28af2ea9
tcyubahi <tcyubahi@asu.edu>
2018-11-14 05:12:32 +0000

    Added classes for notifications, methods, and rest point for getting notifications

--- setup/app/modules.py
+++ setup/app/modules.py
@@ -1,3 +1,7 @@
+'''
+    Author: ASU Capstone Team 2018
+    Description: Classes and modules implementations
+'''
 from pymongo import MongoClient
 from pprint import pprint
 from bson.json_util import dumps
@@ -5,6 +9,8 @@ from flask import url_for
 import json
 import traceback
 
+client = MongoClient("mongodb://0.0.0.0:27017")
+db = client.solarsensereports
 
 class SoildDataCollection(object):
 
@@ -17,9 +23,9 @@ class SoildDataCollection(object):
 
     def getData(self):
         try:
-            client = MongoClient("mongodb://0.0.0.0:27017")
+            #client = MongoClient("mongodb://0.0.0.0:27017")
             #client = MongoClient("mongodb://localhost:27017")
-            db = client.solarsensereports
+            #db = client.solarsensereports
             reports = db.reports
             jsonObj = reports.find()
             print("[DEBUG MODULES] ")
@@ -49,4 +55,49 @@ class SoilDataModel(object):
 
     def getSoilData(self):
         return self.__dataValues
+
+'''
+Notifications class
+'''
+class Notifications(object):
+    """This is a class for all app's instance Notifications"""
+    def __init__(self):
+        self.allNotifications = []
+
+    class Notification(object):
+        """ This is a class for a notification """
+        def __init__(self, content, type, timestamp):
+            self.content = content
+            self.type = type
+            self.timestamp = timestamp
+        def toString(self):
+            return json.dumps(self)
+
+        """ method to get new notifications """
+    def getNewNotifications(self):
+        try:
+            notifications = db.notifications
+            newNotifications = notifications.find()
+            for newNotification in newNotifications:
+                notif = Notification(newNotification.content, newNotification.type, newNotification.timestamp)
+                self.allNotifications.append(newNotification)
+        except Exception as e:
+            file = open("errorlog.txt", "a")
+            file.write(traceback.format_exc())
+            file.close()
+
+        """ method to save a new notification """
+    def saveNewNotifications(self, content, type, timestamp):
+        try:
+            notifications = db.notifications
+            newNotification = Notification(content, type, timestamp)
+            toJson = json.loads(newNotification.toString()) 
+            result = notifications.insert_one(toJson)
+
+        except Exception as e:
+            file = open("errorlog.txt", "a")
+            file.write(traceback.format_exc())
+            file.close()
+            
+
 
--- setup/app/routes.py
+++ setup/app/routes.py
@@ -1,3 +1,7 @@
+'''
+    Author: ASU Capstone Team 2018
+    Description: Routes and endpoints for the solarsense REST API
+'''
 import subprocess
 import os
 import traceback
@@ -6,12 +10,14 @@ from pathlib import Path
 from app import app
 from app.forms import HomeForm
 from app.modules import SoildDataCollection
+from app.modules import Notifications
 from flask import render_template, make_response
 from flask_jsonpify import jsonify
 from flask_cors import cross_origin
 from bson.json_util import dumps
 
 
+''' =============== ROUTES ================ '''
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
@@ -20,6 +26,14 @@ def home():
 def instant():
     return render_template('instant.html')
 
+@app.route('/scan')
+def scan():
+    return render_template('scan.html')
+
+''' =============== END OF ROUTES ================ '''
+
+
+''' =============== ENDPOINTS ================ '''
 @app.route('/data', methods=['GET'])
 @cross_origin()
 def data():
@@ -41,10 +55,33 @@ def data():
         'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With'
         })
 
-@app.route('/scan')
-def scan():
-    return render_template('scan.html')
+    
+    """ end point for retrieving notifications """
+@app.route('/get/notifications', methods=['GET'])
+@cross_origin()
+def data():
+    newNotifications = []
+    notifications = Notifications()
+    for newNotification in notifications.getNewNotifications():
+        toString = json.dumps(newNotification)
+        newNotifications.append(toString)
 
+    return make_response(jsonify(newNotifications),200,{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods' : 'PUT,GET',
+        'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With'
+        })
+
+''' =============== END OF ENDPOINTS ================ '''
+
+
+
+''' =============== TEST END POINTS ================ '''
+
+''' =============== END OF TEST ENDPOINTS ================ '''
+
+
+''' =============== ERROR HANDLERS ================ '''
 @app.errorhandler(500)
 def internal_error(error):
     file = open("errorlog.txt", "a")
@@ -59,3 +96,4 @@ def resource_not_found(error):
     file.write(traceback.format_exc())
     file.close()
     return traceback.format_exc()
+''' =============== END OF ERROR HANDLERS ================ '''
