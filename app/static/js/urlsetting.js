6a3b8aa4f6ea907363053ed35d23f4499a009944
eotetb <jortner1@asu.edu>
2019-02-19 17:39:09 -0700

    US233 attempt to create config files to create global variable for URL

---
+++ app/static/js/urlsetting.js
@@ -0,0 +1,38 @@
+var app = angular.module('solarsenseApp', []);
+
+app.run(function ($rootScope) {
+    $rootScope.domainName = "http://localhost:5000";
+});
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  	$interpolateProvider.startSymbol('{a');
+  	$interpolateProvider.endSymbol('a}');
+}]);
+
+app.controller('LinkCtrl', function($scope, $window) {
+
+  	$scope.startCollection = function () {
+		$window.location.href = "instant";
+	}
+
+	$scope.test = function(){
+		$scope.gettingAlgorithm();
+		$scope.gettingAlgorithmFromSensors();
+	}
+
+	$scope.scanSensors = function () {
+		$window.location.href = "scan";
+	}
+
+	$scope.goToLearn = function () {
+		$window.location.href = "learn";
+	}
+
+	$scope.goToFarmStatus = function () {
+		$window.location.href = "/";
+	}
+
+	$scope.openConfig = function () {
+		$window.location.href = "config";
+	}
+});
