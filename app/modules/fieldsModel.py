aae23de91096653f98369dcf79a8a43c30f46cf5
tcyubahi <tcyubahi@asu.edu>
2019-03-23 20:20:31 -0700

    Fetching fields from Mongo

--- app/modules/fieldsModel.py
+++ app/modules/fieldsModel.py
@@ -16,16 +16,18 @@ class FieldsCollection(object):
     def __init__(self):
         self.fieldsList = []
         self.client = MongoClient("mongodb://0.0.0.0:27017")
-        self.db = self.client.solarsensereports
+        self.db = self.client.FarmInfo
+        self.collection = self.db.fields
 
     def getFields(self):
         self.getAll()
         return self.fieldsList
 
     def getAll(self):
-        for i in range(4):
-            oneField = Field(i, 'Field_' + str(i))
-            self.fieldsList.append(oneField)
+        allFields = self.collection.find()
+        for aField in allFields:
+                oneField = Field(str(aField['_id']), str(aField['name']))
+                self.fieldsList.append(oneField)
 
     def close(self):
         self.client.close()
