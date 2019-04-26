808d4e1326c36d0644ff0f408bca968992803f89
wjd345 <wjdavis1@asu.edu>
2019-04-26 00:45:44 -0700

    Fixed date generation in data file

--- Database_Structures/generate_test_data.py
+++ Database_Structures/generate_test_data.py
@@ -1,14 +1,15 @@
 import datetime
+from dateutil.tz import tzlocal
 
 file = open("./DATAFILES/sensorDataTest.json", "a")
 temp = 21.0
 light = 1000
 moisture = 50
-date = 1551978253
+date = 1556236800 + 25200
 conduct = 1000
 for day in range(7):
   for half_hour in range(48):
-    file.write('{{"temperature": {0}, "battery": 100, "light": {1}, "mac": "C4:7C:8D:66:CF:40", "moisture": {2}, "timestamp": {{"$date": {3}}}, "name_pretty": "Flora-care0", "conductivity": {4}}}'.format(temp, int(light), int(moisture), datetime.datetime.fromtimestamp(date/1e3).isoformat(), conduct))
+    file.write('{{"temperature": {0}, "battery": 100, "light": {1}, "mac": "C4:7C:8D:66:CF:40", "moisture": {2}, "timestamp": {{"$date": \"{3}\"}}, "name_pretty": "Flora-care0", "conductivity": {4}}}'.format(temp, int(light), int(moisture), datetime.datetime.fromtimestamp(date,tzlocal()).isoformat(), conduct))
     if (half_hour < 12): 
       temp += 0
       light += 10
@@ -26,4 +27,4 @@ for day in range(7):
       conduct += 30
     date += 1800
     if ((day*half_hour != 282)):
-      file.write(',\n')
+      file.write('\n')
